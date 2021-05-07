933946ad1c70b1ccdab8339b5995210bffa81d33
eliarbogast <arbogaste@carleton.edu>
2021-05-07 13:28:16 -0500

    completed Part 1

--- authenication/hasher.py
+++ authenication/hasher.py
@@ -9,7 +9,7 @@ words = [line.strip().lower() for line in open('words.txt')]
 
 # Compute the MD5 hash of this example password
 password = 'moose' # type=string
-passwords = [line.strip().lower() for line in open('passwords1.txt')]
+passwords = [line.strip().lower() for line in open('passwords_hash.txt')]
 userpassList = {}
 
 #Create dictonary of key/value pairs
@@ -19,17 +19,41 @@ for line in passwords:
     pword = info[1]
     userpassList[username] = pword
 #print(userpassList)
+
 #create dictionary of {key, value} pairs that consist of 
 # {hash, password}
 # we'll have to encode/hash/hasashexstring for each word in words.txt
 # we can then check each pword from userpassList against the keys in our new dictionary
+
+hashDict = {}
+for line in words:
+    encodedPassword = line.encode('utf-8') # type=bytes
+    md5 = hashlib.md5(encodedPassword)
+    passwordHash = md5.digest() # type=bytes
+    passwordHashAsHex = binascii.hexlify(passwordHash) # weirdly, still type=bytes
+    passwordHashAsHexString = passwordHashAsHex.decode('utf-8') # type=string
+    hashDict[passwordHashAsHexString] = line
+
+crackedPasswords = {}
+for key in userpassList:
+    pword = userpassList[key]
+    if pword in hashDict: 
+        crackedPasswords[key] = hashDict[pword] 
+
+passFile = open("passwords1.txt", "w")
+for line in crackedPasswords:
+    passFile.write(line + ":" + crackedPasswords[line] + "\n")
+
+print(len(hashDict))
+
+'''
 for key in userpassList:
     passwordHash = userpassList[key]
     passwordHashAsHex = binascii.hexlify(passwordHash) # weirdly, still type=bytes
     passwordHashAsHexString = passwordHashAsHex.decode('utf-8') # type=string
     passwordString = passwordHashasHexString.decode('hex') # type=string
     print passwordString
-
+'''
 
 '''
 print('password ({0}): {1}'.format(type(password), password))
